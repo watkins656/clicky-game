a8c435667fd2aa01a189b0bdece529e6190bcba0
Dustin Watkins <watkins656@gmail.com>
2019-01-29 23:33:15 -0600

    clicky

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+This will soon be the Clicky Game!
           </p>
           <a
             className="App-link"
